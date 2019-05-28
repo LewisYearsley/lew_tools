83b12bbb312dddb2bd3a709f56525e1414e6d6c7
Lewis Yearsley <lewis.yearsley@arcadiagroup.co.uk>
2019-05-28 20:33:41 +0100

    Initial commit

---
+++ update_with_base.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+base_branch=$1
+current_branch=$(git branch | grep \*)
+clean_branch=${current_branch/[*\s\n]}
+
+if [ "$clean_branch" == "$base_branch" ] || [ -z "$base_branch" ]
+then
+    echo "Pulling latest $clean_branch"
+    git pull --rebase
+else
+    echo "Updating $clean_branch with $base_branch"
+    echo $clean_branch
+
+    git checkout develop
+    git pull --rebase
+    git checkout $clean_branch
+    git merge develop
+fi
